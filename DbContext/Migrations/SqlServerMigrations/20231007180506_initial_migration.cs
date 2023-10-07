318b8fac19352320ad681cef7944203a5fbe7ee3
Pingeln <niklasmartinsson89@gmail.com>
2023-10-07 20:07:13 +0200

    fine tuning

---
+++ DbContext/Migrations/SqlServerMigrations/20231007180506_initial_migration.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbContext.Migrations.SqlServerMigrations
+{
+    /// <inheritdoc />
+    public partial class initial_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                schema: "dbo",
+                columns: table => new
+                {
+                    AddressID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    ZipCode = table.Column<string>(type: "nvarchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.AddressID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "dbo",
+                columns: table => new
+                {
+                    UserID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attractions",
+                schema: "dbo",
+                columns: table => new
+                {
+                    AttractionID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    AddressID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attractions", x => x.AttractionID);
+                    table.ForeignKey(
+                        name: "FK_Attractions_Addresses_AddressID",
+                        column: x => x.AddressID,
+                        principalSchema: "dbo",
+                        principalTable: "Addresses",
+                        principalColumn: "AddressID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                schema: "dbo",
+                columns: table => new
+                {
+                    CommentID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    UserID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AttractionID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.CommentID);
+                    table.ForeignKey(
+                        name: "FK_Comments_Attractions_AttractionID",
+                        column: x => x.AttractionID,
+                        principalSchema: "dbo",
+                        principalTable: "Attractions",
+                        principalColumn: "AttractionID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Comments_Users_UserID",
+                        column: x => x.UserID,
+                        principalSchema: "dbo",
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attractions_AddressID",
+                schema: "dbo",
+                table: "Attractions",
+                column: "AddressID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AttractionID",
+                schema: "dbo",
+                table: "Comments",
+                column: "AttractionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserID",
+                schema: "dbo",
+                table: "Comments",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Attractions",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Addresses",
+                schema: "dbo");
+        }
+    }
+}
